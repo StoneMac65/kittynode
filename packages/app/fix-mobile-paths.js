7c78769b5601f6ac232aa150e083ae8273986d38
StoneMac65 <stonemac65@gmail.com>
2025-11-13 20:05:59 +0000

    Fix: Add src attribute replacement in fix-mobile-paths.js for Android assets

--- packages/app/fix-mobile-paths.js
+++ packages/app/fix-mobile-paths.js
@@ -7,7 +7,9 @@ const indexPath = join(__dirname, 'build/index.html');
 let html = readFileSync(indexPath, 'utf-8');
 
 // Replace absolute paths with relative paths
+// Handle all /app/ paths (including /app/immutable/...)
 html = html.replace(/href="\/app\//g, 'href="./app/');
+html = html.replace(/src="\/app\//g, 'src="./app/');
 html = html.replace(/import\("\/app\//g, 'import("./app/');
 html = html.replace(/href="\/favicon\.ico"/g, 'href="./favicon.ico"');
 
